71cc5b3d885e46d02c28f171dc0518bd3e54379a
vikash <mukthavikash@gmail.com>
2023-04-02 23:38:00 +0530

    Image Scan with v1 in ECR for Demo purpose

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -9,7 +9,7 @@
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-    trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
+    trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:v1 > scan.txt
     cat scan.txt
     """
 }
