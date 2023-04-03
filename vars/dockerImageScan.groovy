4bfc8a3ab8df5aa76f66d65862ff7ed055735e4a
vikash <mukthavikash@gmail.com>
2023-04-03 15:23:54 +0530

    Images with new v0 - v3 changes

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -9,7 +9,7 @@
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-    trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:v0 > scan.txt
+    trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:v3 > scan.txt
     cat scan.txt
     """
 }
