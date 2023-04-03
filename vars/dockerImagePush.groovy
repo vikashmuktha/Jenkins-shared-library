6a8ebd5ded8e417eee890f999b53b7f3d1051807
vikash <mukthavikash@gmail.com>
2023-04-03 14:46:10 +0530

    Images with new v2 - v0 changes

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -15,6 +15,6 @@ def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
      aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:v2
+     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:v0
     """
 }
