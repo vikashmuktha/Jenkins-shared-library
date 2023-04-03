2b2f24ae26f6c52c76e4919f2e1f1c555feffbd7
vikash <mukthavikash@gmail.com>
2023-04-03 11:45:00 +0530

    Images with new v1 - v2 changes

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -11,6 +11,6 @@ def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
      docker build -t ${ecr_repoName} .
-     docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:v1
+     docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:v2
     """
 }
